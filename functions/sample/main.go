3a1d62682044c3cf5dd812eab31f1b297024ac93
namsupark <skatn7979@gmail.com>
2024-11-17 18:11:53 +0900

    api

--- functions/sample/main.go
+++ functions/sample/main.go
@@ -1,1 +1,17 @@
-package sample
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		Body:       "Sample API",
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
